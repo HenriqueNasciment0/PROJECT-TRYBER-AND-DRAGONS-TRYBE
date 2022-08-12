808263943a461b3a79946410308042e1997c9c2a
HenriqueNasciment0 <you@example.com>
2022-08-12 10:02:34 -0300

    subindo novamente pra ver se vai, alteração no PVE

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -18,8 +18,9 @@ export default class PVE extends Battle {
   public fight(): number {
     if (this._character.lifePoints > 0
          && this._environment.some((monster) => monster.lifePoints > 0)) {
-      this._environment.map((monster) => monster.attack(this._character));
-      this._environment.map((monster) => this._character.attack(monster));
+      this._environment.forEach((monster) => monster.attack(this._character));
+      this._environment.forEach((_monster, index) =>
+        this._character.attack(this._environment[index]));
     }
     return this._character.lifePoints === -1 ? -1 : 1;
   }
